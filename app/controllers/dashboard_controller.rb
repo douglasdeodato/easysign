501f1bb26da90c3634d1c7e6d9817ff8ec52dca8
Douglas Deodato <douglasdeodato@gmail.com>
2015-05-11 09:36:13 +0100

    done

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,6 @@
 class DashboardController < ApplicationController
 end
+before_filter :authenticate_user!
+ 
+def index    
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
 end
+
+def index
+  if user_signed_in?
+    redirect_to :controller=&amp;gt;'dashboard', :action =&amp;gt; 'index'
+  end
+end
