dee0411a047f17e155bed82e811aee2b4e219e2c
Douglas Deodato <douglasdeodato@gmail.com>
2015-05-12 18:43:18 +0100

    Added server scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :dashboard
   root to: "home#index"
+  #root 'servers#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511082938) do
+ActiveRecord::Schema.define(version: 20150512173834) do
+
+  create_table "servers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
